6de2039b038daf19d2e49e52742bdfe5f7893378
Diana <ivdiana14500@gmail.com>
2025-05-29 09:47:14 +0300

    Updates

--- assets/firebase-DNVwFFU9.js
+++ assets/firebase-DNVwFFU9.js
@@ -1,4 +1,4 @@
-import{r}from"./@firebase-CjYIGFut.js";var e="firebase",a="10.9.0";/**
+import{r}from"./@firebase-DDPdATnU.js";var e="firebase",a="10.9.0";/**
  * @license
  * Copyright 2020 Google LLC
  *
